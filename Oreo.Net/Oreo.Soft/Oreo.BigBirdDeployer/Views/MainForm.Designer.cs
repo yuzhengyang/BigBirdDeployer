1f5cef36a08e978061e40a892fa6d6c4d76df59d
yuzhengyang <inc@live.cn>
2018-05-06 22:50:17 +0800

    从Fork项目中移除的Oreo软件，单独分离到新仓库

---
+++ Oreo.Net/Oreo.Soft/Oreo.BigBirdDeployer/Views/MainForm.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Oreo.BigBirdDeployer.Views
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.project1 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.project4 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.project2 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.project3 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.project6 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.project5 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.project7 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.project8 = new Oreo.BigBirdDeployer.Parts.ProjectItemPart();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LBDesc = new System.Windows.Forms.Label();
+            this.BTCancel = new System.Windows.Forms.Button();
+            this.TBNewStorage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBPublishStorage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.BigIconFormPBHeadIcon)).BeginInit();
+            this.BigIconFormPNContainer.SuspendLayout();
+            this.BigIconFormPNHead.SuspendLayout();
+            this.BigIconFormPNHeadButton.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BigIconFormLBHeadTitle
+            // 
+            this.BigIconFormLBHeadTitle.ForeColor = System.Drawing.Color.White;
+            this.BigIconFormLBHeadTitle.Size = new System.Drawing.Size(508, 68);
+            this.BigIconFormLBHeadTitle.Text = "Big Bird Deployer Java 服务启动管理工具";
+            // 
+            // BigIconFormPNContainer
+            // 
+            this.BigIconFormPNContainer.BackColor = System.Drawing.Color.White;
+            this.BigIconFormPNContainer.Controls.Add(this.tabControl1);
+            this.BigIconFormPNContainer.Size = new System.Drawing.Size(775, 445);
+            // 
+            // BigIconFormPNHead
+            // 
+            this.BigIconFormPNHead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(120)))), ((int)(((byte)(174)))));
+            this.BigIconFormPNHead.Size = new System.Drawing.Size(775, 68);
+            // 
+            // BigIconFormPNHeadButton
+            // 
+            this.BigIconFormPNHeadButton.Location = new System.Drawing.Point(658, 0);
+            // 
+            // BigIconFormBTFormMinBox
+            // 
+            this.BigIconFormBTFormMinBox.FlatAppearance.BorderSize = 0;
+            this.BigIconFormBTFormMinBox.ForeColor = System.Drawing.Color.White;
+            this.BigIconFormBTFormMinBox.Location = new System.Drawing.Point(44, 7);
+            // 
+            // BigIconFormBTFormMaxBox
+            // 
+            this.BigIconFormBTFormMaxBox.FlatAppearance.BorderSize = 0;
+            this.BigIconFormBTFormMaxBox.ForeColor = System.Drawing.Color.White;
+            this.BigIconFormBTFormMaxBox.Location = new System.Drawing.Point(82, 36);
+            this.BigIconFormBTFormMaxBox.Visible = false;
+            // 
+            // BigIconFormBTFormCloseBox
+            // 
+            this.BigIconFormBTFormCloseBox.FlatAppearance.BorderSize = 0;
+            this.BigIconFormBTFormCloseBox.ForeColor = System.Drawing.Color.White;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 445);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.tabPage1.Controls.Add(this.project1);
+            this.tabPage1.Controls.Add(this.project4);
+            this.tabPage1.Controls.Add(this.project2);
+            this.tabPage1.Controls.Add(this.project3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 419);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = " 【第一页】 ";
+            // 
+            // project1
+            // 
+            this.project1.BackColor = System.Drawing.Color.DimGray;
+            this.project1.Location = new System.Drawing.Point(35, 55);
+            this.project1.Name = "project1";
+            this.project1.Size = new System.Drawing.Size(324, 116);
+            this.project1.TabIndex = 0;
+            // 
+            // project4
+            // 
+            this.project4.BackColor = System.Drawing.Color.DimGray;
+            this.project4.Location = new System.Drawing.Point(403, 204);
+            this.project4.Name = "project4";
+            this.project4.Size = new System.Drawing.Size(324, 116);
+            this.project4.TabIndex = 4;
+            // 
+            // project2
+            // 
+            this.project2.BackColor = System.Drawing.Color.DimGray;
+            this.project2.Location = new System.Drawing.Point(403, 55);
+            this.project2.Name = "project2";
+            this.project2.Size = new System.Drawing.Size(324, 116);
+            this.project2.TabIndex = 1;
+            // 
+            // project3
+            // 
+            this.project3.BackColor = System.Drawing.Color.DimGray;
+            this.project3.Location = new System.Drawing.Point(35, 204);
+            this.project3.Name = "project3";
+            this.project3.Size = new System.Drawing.Size(324, 116);
+            this.project3.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(199)))), ((int)(((byte)(255)))));
+            this.tabPage2.Controls.Add(this.project6);
+            this.tabPage2.Controls.Add(this.project5);
+            this.tabPage2.Controls.Add(this.project7);
+            this.tabPage2.Controls.Add(this.project8);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(767, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = " 【不够再来一页】 ";
+            // 
+            // project6
+            // 
+            this.project6.BackColor = System.Drawing.Color.DimGray;
+            this.project6.Location = new System.Drawing.Point(403, 55);
+            this.project6.Name = "project6";
+            this.project6.Size = new System.Drawing.Size(324, 116);
+            this.project6.TabIndex = 14;
+            // 
+            // project5
+            // 
+            this.project5.BackColor = System.Drawing.Color.DimGray;
+            this.project5.Location = new System.Drawing.Point(35, 55);
+            this.project5.Name = "project5";
+            this.project5.Size = new System.Drawing.Size(324, 116);
+            this.project5.TabIndex = 13;
+            // 
+            // project7
+            // 
+            this.project7.BackColor = System.Drawing.Color.DimGray;
+            this.project7.Location = new System.Drawing.Point(35, 204);
+            this.project7.Name = "project7";
+            this.project7.Size = new System.Drawing.Size(324, 116);
+            this.project7.TabIndex = 12;
+            // 
+            // project8
+            // 
+            this.project8.BackColor = System.Drawing.Color.DimGray;
+            this.project8.Location = new System.Drawing.Point(403, 204);
+            this.project8.Name = "project8";
+            this.project8.Size = new System.Drawing.Size(324, 116);
+            this.project8.TabIndex = 11;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.LBDesc);
+            this.tabPage3.Controls.Add(this.BTCancel);
+            this.tabPage3.Controls.Add(this.TBNewStorage);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.TBPublishStorage);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.BTSave);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(767, 419);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = " 【设置】 ";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(437, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 12);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "默认为程序目录下 Storage\\New";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(437, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 12);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "默认为程序目录下 Storage\\Publish";
+            // 
+            // LBDesc
+            // 
+            this.LBDesc.AutoSize = true;
+            this.LBDesc.Location = new System.Drawing.Point(69, 191);
+            this.LBDesc.Name = "LBDesc";
+            this.LBDesc.Size = new System.Drawing.Size(77, 12);
+            this.LBDesc.TabIndex = 23;
+            this.LBDesc.Text = "执行结果描述";
+            // 
+            // BTCancel
+            // 
+            this.BTCancel.Location = new System.Drawing.Point(559, 265);
+            this.BTCancel.Name = "BTCancel";
+            this.BTCancel.Size = new System.Drawing.Size(75, 23);
+            this.BTCancel.TabIndex = 22;
+            this.BTCancel.Text = "取消";
+            this.BTCancel.UseVisualStyleBackColor = true;
+            // 
+            // TBNewStorage
+            // 
+            this.TBNewStorage.Location = new System.Drawing.Point(188, 105);
+            this.TBNewStorage.Name = "TBNewStorage";
+            this.TBNewStorage.Size = new System.Drawing.Size(235, 21);
+            this.TBNewStorage.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "新增资料库路径";
+            // 
+            // TBPublishStorage
+            // 
+            this.TBPublishStorage.Location = new System.Drawing.Point(188, 50);
+            this.TBPublishStorage.Name = "TBPublishStorage";
+            this.TBPublishStorage.Size = new System.Drawing.Size(235, 21);
+            this.TBPublishStorage.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "发布资料库路径";
+            // 
+            // BTSave
+            // 
+            this.BTSave.Location = new System.Drawing.Point(427, 265);
+            this.BTSave.Name = "BTSave";
+            this.BTSave.Size = new System.Drawing.Size(75, 23);
+            this.BTSave.TabIndex = 17;
+            this.BTSave.Text = "保存";
+            this.BTSave.UseVisualStyleBackColor = true;
+            this.BTSave.Click += new System.EventHandler(this.BTSave_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 513);
+            this.DoubleClickMax = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Controls.SetChildIndex(this.BigIconFormPNHead, 0);
+            this.Controls.SetChildIndex(this.BigIconFormPNContainer, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.BigIconFormPBHeadIcon)).EndInit();
+            this.BigIconFormPNContainer.ResumeLayout(false);
+            this.BigIconFormPNHead.ResumeLayout(false);
+            this.BigIconFormPNHeadButton.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Parts.ProjectItemPart project1;
+        private Parts.ProjectItemPart project2;
+        private Parts.ProjectItemPart project3;
+        private Parts.ProjectItemPart project4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private Parts.ProjectItemPart project7;
+        private Parts.ProjectItemPart project8;
+        private System.Windows.Forms.TabPage tabPage3;
+        private Parts.ProjectItemPart project6;
+        private Parts.ProjectItemPart project5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LBDesc;
+        private System.Windows.Forms.Button BTCancel;
+        private System.Windows.Forms.TextBox TBNewStorage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBPublishStorage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTSave;
+    }
+}
---
+++ Oreo.Net/Oreo.Soft/Oreo.BigBirdDeployer/Views/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using Azylee.Core.IOUtils.TxtUtils;
+using Azylee.WinformSkin.FormUI.CustomTitle;
+using Oreo.BigBirdDeployer.Commons;
+using Oreo.BigBirdDeployer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Oreo.BigBirdDeployer.Views
+{
+    public partial class MainForm : BigIconForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            TBPublishStorage.Text = R.Paths.PublishStorage;
+            TBNewStorage.Text = R.Paths.NewStorage;
+        }
+
+        private void BTSave_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+        }
+
+        #region 方法
+        private bool SaveSettings()
+        {
+            bool flag = false;
+            if (!string.IsNullOrWhiteSpace(TBPublishStorage.Text))
+            {
+                if (Directory.Exists(TBPublishStorage.Text))
+                {
+                    R.Paths.PublishStorage = TBPublishStorage.Text;
+                    IniTool.WriteValue(R.Files.Settings, "Paths", "PublishStorage", R.Paths.PublishStorage);
+                    flag = true;
+                }
+                else
+                {
+                    LBDesc.Text = "发布资料库目录不存在";
+                }
+            }
+            else
+            {
+                R.Paths.PublishStorage = R.Paths.DefaultPublishStorage;
+                IniTool.WriteValue(R.Files.Settings, "Paths", "PublishStorage", R.Paths.PublishStorage);
+                flag = true;
+            }
+
+            if (!string.IsNullOrWhiteSpace(TBNewStorage.Text))
+            {
+                if (Directory.Exists(TBNewStorage.Text))
+                {
+                    R.Paths.NewStorage = TBNewStorage.Text;
+                    IniTool.WriteValue(R.Files.Settings, "Paths", "NewStorage", R.Paths.NewStorage);
+                    flag = true;
+                }
+                else
+                {
+                    LBDesc.Text = "新增资料库目录不存在";
+                }
+            }
+            else
+            {
+                R.Paths.NewStorage = R.Paths.DefaultNewStorage;
+                IniTool.WriteValue(R.Files.Settings, "Paths", "NewStorage", R.Paths.NewStorage);
+                flag = true;
+            }
+            return flag;
+        }
+        #endregion
+    }
+}
